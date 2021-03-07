bfa72d08fb81f33327e422824f8ac87171ea9f6e
Sergio GMQ <sergiogilga@gmail.com>
2021-03-07 13:15:36 +0100

    Metodo contador de numeros

--- src/ejercicios/Metodos.java
+++ src/ejercicios/Metodos.java
@@ -4,16 +4,40 @@ public class Metodos {
 
 	public static void main(String[] args) {
 		
+//		imprimirHolaMundo();
+//		imprimirUnaLetraConChar();
+//		imprimirUnNumeroConChar();
+		variablesInt();
+	}
+	
+	static void imprimirHolaMundo() {
+		
 		String palabra1 = "Hola";
 		String palabra2 = "Mundo";
 		System.out.println(palabra1 + " " + palabra2);
+	}
+	
+	static void imprimirUnaLetraConChar() {
 		
 		char numero = 97;
 		System.out.println(numero);
+	}
+	
+	static void imprimirUnNumeroConChar() {
 		
 		char letra = 'a';
 		double c = letra;
 		System.out.println(c);
 	}
 
+	static void variablesInt() {
+		
+		int comienzo = 23;
+		int resta = 2;
+
+		for (int i = Math.abs(comienzo); i > 0; i -= Math.abs(resta)) {
+
+			System.out.println(i);
+		}
+	}
 }
